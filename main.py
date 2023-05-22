0924dc4c6169e2920c8baef21d7e0ac23e281b6c
Transformice2000 <129300413+Transformice2000@users.noreply.github.com>
2023-05-22 13:15:10 +0300

    Second task

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
 class Student:
     def __init__(self, name, surname, gender):
-        self.name = name  # Имя студента
-        self.surname = surname  # Фамилия студента
-        self.gender = gender  # Пол студента
-        self.finished_courses = []  # Список завершенных курсов
-        self.courses_in_progress = []  # Список курсов, на которых студент обучается в данный момент
-        self.grades = {}  # Оценки студента
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def __str__(self):
+        avg_grade = sum(sum(course_grades) / len(course_grades) for course_grades in self.grades.values()) / len(self.grades)
+        # Считаем среднюю оценку за домашние задания
+        courses_in_progress_str = ", ".join(self.courses_in_progress)
+        finished_courses_str = ", ".join(self.finished_courses)
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {avg_grade:.1f}\nКурсы в процессе изучения: {courses_in_progress_str}\nЗавершенные курсы: {finished_courses_str}"
+
 
 class Mentor:
     def __init__(self, name, surname):
-        self.name = name  # Имя преподавателя
-        self.surname = surname  # Фамилия преподавателя
-        self.courses_attached = []  # Список курсов, которые преподаватель ведет
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            # Проверяем, что переданный объект является экземпляром класса Student,
-            # курс преподавателя прикреплен к студенту и студент обучается на этом курсе
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
+
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.lecture_grades = {}  # Оценки за лекции
+        self.lecture_grades = {}
+
+    def __str__(self):
+        avg_grade = sum(sum(course_grades) / len(course_grades) for course_grades in self.lecture_grades.values()) / len(self.lecture_grades)
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg_grade:.1f}"
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            # Проверяем, что переданный объект является экземпляром класса Student,
-            # курс проверяющего прикреплен к студенту и студент обучается на этом курсе
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -41,14 +54,19 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')  # Создание объекта студента
-best_student.courses_in_progress += ['Python']  # Добавление курса в список курсов студента
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nУ лекторов:"
 
-cool_mentor = Mentor('Some', 'Buddy')  # Создание объекта преподавателя
-cool_mentor.courses_attached += ['Python']  # Добавление курса в список курсов преподавателя
 
-cool_mentor.rate_hw(best_student, 'Python', 10)  # Выставление оценки студенту
-cool_mentor.rate_hw(best_student, 'Python', 10)  # Выставление оценки студенту
-cool_mentor.rate_hw(best_student, 'Python', 10)  # Выставление оценки студенту
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
 
-print(best_student.grades)
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+print(best_student)
+print(cool_mentor)
